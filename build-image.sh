6f4d358761b99d802a61b1f83b427d88ce58a919
achraf <achraf.pica@gmail.com>
2020-09-24 21:23:20 +0200

    add hook pipeline

--- build-image.sh
+++ build-image.sh
@@ -6,3 +6,7 @@ JAR_FILE=$(ls build/libs/ | grep "^finance-forex")
 
 docker build . --build-arg jar=build/libs/$JAR_FILE -t ezzefiohez/finance-forex
 docker push ezzefiohez/finance-forex
+
+echo " ######## BUILD FOREX DONE ######## "
+
+curl  -X POST http://94.239.109.172:9000/api/webhooks/83431043-97e7-4f1a-a341-d2214c1f93c2
